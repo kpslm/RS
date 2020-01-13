209d82a76b06e3a381e2d94ff0816f020d680e5a
loux22 <louis.ardilly@yahoo.fr>
2020-01-13 14:47:21 +0100

    add navbar

---
+++ RS/src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('user/register.html.twig', []);
+    }
+}
