5fa92d46e92b0068dad78154de5500027790cff2
YaminaLouisThomas <yamina.gherbi@ynov.com>
2020-02-05 15:47:15 +0100

    addlogin

--- RS/src/Controller/UserController.php
+++ RS/src/Controller/UserController.php
@@ -7,6 +7,8 @@ use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -16,33 +18,35 @@ class UserController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request)
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-
+        $user = $this->getUser();
+        if ( $user!= null) {
+            return $this->redirectToRoute('profil');
+        }
         $user = new User;
-        $form = $this -> createForm(UserType::class, $user);
-        $form ->handleRequest($request);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-        if($form -> isSubmitted() && $form ->isValid()){
-            $user ->setAtCreated(new \DateTime('now'));
-            $user ->setIsActivate(true);
-
-
-            $manager = $this ->getDoctrine() ->getManager();
-            $manager -> persist($user);
-           
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setAtCreated(new \DateTime('now'));
+            $user->setIsActivate(true);
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
             
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
 
-            $manager ->flush();
 
-            $this ->addFlash('success',' Bienvenue parmis nous ' .$user -> getPrenom() . ' tu es desormais inscris');
+
+            $manager->flush();
+
+            $this->addFlash('success', ' Bienvenue parmis nous ' . $user->getPrenom() . ' tu es desormais inscris');
             //afficher la vue 
-            return $this -> redirectToRoute('login');
-
-
+            return $this->redirectToRoute('login');
         }
 
-        return $this->render('user/register.html.twig', ['RegisterForm'=>$form -> createView()]);
+        return $this->render('user/register.html.twig', ['RegisterForm' => $form->createView()]);
     }
 
     /**
@@ -50,14 +54,25 @@ class UserController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils)
     {
+
+        $user = $this->getUser();
+        if ($user != null) {
+            return $this->redirectToRoute('profil');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
         return $this->render('user/login.html.twig', [
-            'error' => $error
+            'error' => $error,
         ]);
-        
     }
 
-      /**
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    }
+
+    /**
      * @Route("/profil", name="profil")
      */
     public function profil()
@@ -65,7 +80,7 @@ class UserController extends AbstractController
         return $this->render('user/profil.html.twig', []);
     }
 
-       /**
+    /**
      * @Route("/galery", name="galery")
      */
     public function galery()
@@ -73,7 +88,7 @@ class UserController extends AbstractController
         return $this->render('user/galery.html.twig', []);
     }
 
-      /**
+    /**
      * @Route("/picture", name="picture")
      */
     public function picture()
@@ -81,7 +96,7 @@ class UserController extends AbstractController
         return $this->render('user/picture.html.twig', []);
     }
 
-      /**
+    /**
      * @Route("/searchFriends", name="searchFriends")
      */
     public function searchFriends()
@@ -89,7 +104,7 @@ class UserController extends AbstractController
         return $this->render('user/searchFriends.html.twig', []);
     }
 
-     /**
+    /**
      * @Route("/message", name="message")
      */
     public function message()
@@ -97,12 +112,11 @@ class UserController extends AbstractController
         return $this->render('user/message.html.twig', []);
     }
 
-      /**
+    /**
      * @Route("/notification", name="notification")
      */
     public function notification()
     {
         return $this->render('user/notification.html.twig', []);
     }
-
 }
